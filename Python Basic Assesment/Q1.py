be05a772460f2b303b899525aa6a10988da097d7
Sunil Bhave <75983741+Charlie-37@users.noreply.github.com>
2022-11-10 17:34:23 +0530

    Add files via upload

---
+++ Python Basic Assesment/Q1.py
@@ -0,0 +1,14 @@
+''' //*---Print the sum of the current number and the previous number
+numbers = [5, 10, 15, 20, 25, 30, 30]
+Output:
+5
+15
+25
+35
+45
+55
+60'''
+
+numbers = [5, 10, 15, 20, 25, 30, 30]
+for i in range(1:len(numbers)):
+    print(i)
